f8bfe208f9b93015c013c3189e8c1a3915fb90ea
Theodore-Kelechukwu-Onyejiaku <theodoreonyejiaku@gmail.com>
2020-05-26 16:08:35 -0800

    'whatever'

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -2,8 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const Student = mongoose.model("Student");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const {validateRegistration, validateLogin} = require("../middlewares/validation");
 const bcrypt = require("bcryptjs");
+
+
+//Middlewares
+const verify = require("../middlewares/verifyToken")
+
+//Importing the body-parser middle ware
+router.use(bodyparser.urlencoded({
+    extended: true
+}));
+router.use(bodyparser.json())
+//for cors
+router.use(cors());
+//Use of cookies
+router.use(cookieParser());
+
 
 //configuring dotenv file
 require("dotenv").config();
@@ -12,13 +31,27 @@ router.get("/", (req, res)=>{
     res.end("welcoome to the student controller")
 });
 
+//Student Dashboard
+router.get("/",verify,  (req, res) =>{
+    res.render("dashboard")
+})
+
 //Login Page
 router.get("/login", (req, res)=>{
-    res.redirect("/login.html")
+    res.render("login")
 });
+router.get("register", (req, res)=>{
+    res.render("login")
+})
 
 
-router.post("/register", (req, res)=>{
+router.post("/register", (req, res)=>{    
+    //Validate Registration
+    const {error} = validateRegistration(req.body);
+
+    if(error){
+        return  res.render("", {error :error.details[0].message});
+     }
     const student = new Student();
             student.name = req.body.name;
             student.email = req.body.email;
@@ -35,8 +68,27 @@ router.post("/register", (req, res)=>{
                     console.error("Error inserting into database"+ err)
                 }
             })
-})     
+})
 
+router.post("/login", (req, res)=>{
+     //validating the user details before submittin to database
+     const {error} = validateLogin(req.body);
+     if(error){
+       return  res.render("login", {error :error.details[0].message});
+     }
+     User.findOne({email: req.body.email})
+    .then(user =>{
+        console.log(user);
+        var token = jwt.sign(user.toJSON(),"secret",{ expiresIn: '20s' });
+        res.cookie('auth',token);
+        res.redirect("/student/dashboard");
+    })
+    .catch(err =>{
+        console.error(err)
+    })
+
+
+})
 
 
 module.exports = router;
--- controllers/visitorController.js
+++ controllers/visitorController.js
@@ -1,8 +1,46 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
-const Student = mongoose.model("Student");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const {validateRegistration, validateLogin} = require("../middlewares/validation");
+const bcrypt = require("bcryptjs");
 const Course = mongoose.model("Course");
+
+//Middlewares
+const verify = require("../middlewares/verifyToken")
+
+//Importing the body-parser middle ware
+router.use(bodyparser.urlencoded({
+    extended: true
+}));
+router.use(bodyparser.json())
+//for cors
+router.use(cors());
+//Use of cookies
+router.use(cookieParser());
+
+
+//configuring dotenv file
+require("dotenv").config();
+
+//Importing User model
+const Student = mongoose.model("Student");
+
+//Login Page
+router.get("/login", (req, res)=>{
+    res.render("visitor/login", {"layout": ""})
+});
+router.get("/register", (req, res)=>{
+    res.render("visitor/register",{"layout":""})
+})
+
+//Student Dashboard
+router.get("/dashboard",verify,  (req, res) =>{
+    res.render("visitor/dashboard")
+})
 
 router.get("/allCourses", (req, res)=>{
     Course.find((err, doc)=>{
@@ -32,6 +70,81 @@ router.get("/blog", (req, res)=>{
             console.log("Error in fetching data")
         }
     })
+})
+
+
+
+/**
+ *  POST REQUESTS
+ */
+
+router.post("/register", async(req, res)=>{
+    //Validate Registration using Joi
+    const {error} = validateRegistration(req.body);
+
+    if(error){
+        return  res.render("visitor/register", {"layout": "",error :error.details[0].message});
+     }
+
+    //Checking if the user is already in the database
+   const emailExist = await Student.findOne({email: req.body.email});
+   if(emailExist){
+    return  res.render("visitor/register", {"layout": "", error :"Email already exists"});
+   }
+
+    //creating the salt and hashing the password entered
+    const salt = bcrypt.genSaltSync(10)
+    const hashPassword = await bcrypt.hashSync(req.body.password,salt);
+ 
+
+    const student = new Student();
+            student.name = req.body.name;
+            student.email = req.body.email;
+            student.number = req.body.number;
+            student.address = req.body.address;
+            student.dob = req.body.dob;
+            student.username = req.body.username;
+            student.password = hashPassword;
+
+            student.save((err, doc)=>{
+                if(!err){
+                    res.redirect("/login");
+                }else{
+                    console.error("Error inserting into database"+ err)
+                }
+            })
+})     
+
+
+//Login validation
+router.post("/login",async (req, res)=>{
+    //validating the user details before submittin to database
+    const {error} = validateLogin(req.body);
+    if(error){
+      return  res.render("visitor/login", {"layout": "", error :error.details[0].message});
+    }
+
+    const student =await Student.findOne({email: req.body.email})
+
+    if(!student) return  res.render("visitor/login", {"layout": "", error : "Email or password is wrong"});
+    //If the user exists, here we compare password
+    const validPass =  bcrypt.compareSync(req.body.password, student.password)
+    console.log(validPass)
+
+    //If the passwords do not match
+    if(!validPass) return res.render("visitor/login", {"layout": "", error: "Email or password is wrong"});
+      
+
+    Student.findOne({email: req.body.email})
+   .then(student =>{
+       console.log(student);
+       var token = jwt.sign(student.toJSON(),"secret",{ expiresIn: '20s' });
+       res.cookie('auth',token);
+       res.redirect("/dashboard");
+   })
+   .catch(err =>{
+       console.error(err)
+   })
 })
 
 module.exports = router;
